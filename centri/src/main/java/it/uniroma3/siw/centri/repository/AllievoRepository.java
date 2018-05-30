c35391904499dc313cf81b6934583cdfb9903518
Edoardo Montebelli <edomonte1@live.it>
2018-05-30 13:38:37 +0200

    Configurazioni

--- centri/src/main/java/it/uniroma3/siw/centri/repository/AllievoRepository.java
+++ centri/src/main/java/it/uniroma3/siw/centri/repository/AllievoRepository.java
@@ -1,7 +1,6 @@
 package it.uniroma3.siw.centri.repository;
 
 import java.util.Optional;
-
 import org.springframework.data.repository.CrudRepository;
 
 import it.uniroma3.siw.centri.model.Allievo;
