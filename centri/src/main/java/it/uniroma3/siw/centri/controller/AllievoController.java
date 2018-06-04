52e9ab0084c24221b9af2af3a8e8b76e533fefd4
Kaio Cesar Alves Reis <kaiocesar.alvesreis@gmail.com>
2018-06-04 12:53:45 +0200

    Aggiornamenti

--- centri/src/main/java/it/uniroma3/siw/centri/controller/AllievoController.java
+++ centri/src/main/java/it/uniroma3/siw/centri/controller/AllievoController.java
@@ -1,75 +1,64 @@
 package it.uniroma3.siw.centri.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import it.uniroma3.siw.centri.model.Allievo;
 import it.uniroma3.siw.centri.service.AllievoService;
 import it.uniroma3.siw.centri.validator.AllievoValidator;
 
 @Controller
-@RequestMapping("/allievo")
 public class AllievoController {
-
+	
 	@Autowired
-	public AllievoService allievoService;
-
+	private AllievoService allievoService;
+	
 	@Autowired
-	public AllievoValidator allievoValidator;
-
-	@GetMapping("/lista")
-	public String getAllievi(Model model) {
-
-		List<Allievo> allievi = this.allievoService.findAll();
-
-		model.addAttribute("allievi", allievi);
-
-		return "lista-allievi";
+	private AllievoValidator validator;
+	
+	@RequestMapping("/allievi")
+	public String allievi(Model model) {
+		model.addAttribute("allievi",this.allievoService.findAll());
+		return "lista-allievo";
 	}
-
-	@GetMapping("/{email}")
-	public String getAllievo(@PathVariable("email") String email, Model model) {
-
-		model.addAttribute("allievo", this.allievoService.findByEmail(email));
-
+	
+	@RequestMapping("/addAllievo")
+	public String addAllievo(Model model) {
+		model.addAttribute("allievo",new Allievo());
+		return "form-allievo";
+	}
+	
+	@RequestMapping(value = "/allievo/{id}", method = RequestMethod.GET)
+	public String getAllievo(@PathVariable("id") String id, Model model) {
+		model.addAttribute("allievo",this.allievoService.findByEmail(id));
 		return "mostra-allievo";
 	}
-
-	@PostMapping("/nuovoAllievo")
-	public String newAllievo(@Valid @ModelAttribute("allievo") Allievo allievo, Model model,
-			BindingResult bindingResult) {
-
-		this.allievoValidator.validate(allievo, bindingResult);
-
-		if (this.allievoService.existsByEmail(allievo.getEmail())) {
-			model.addAttribute("esistenza", "Allievo già presente");
-			return "form-allievo";
-		} else {
-			if (!bindingResult.hasErrors()) {
-				this.allievoService.save(allievo);
-				model.addAttribute("allievi", allievoService.findAll());
-				return "redirect:/allievo/lista";
-			}
-		}
-		return "form-allievo";
-
-	}
-
-	@GetMapping("/nuovoAllievo")
-	public String newFormAllievo(Allievo allievo) {
-		return "form-allievo";
-
-	}
+	
+	@RequestMapping(value = "/form", method = RequestMethod.POST)
+    public String newCustomer(@Valid @ModelAttribute("allievo") Allievo allievo,BindingResult bindingResult, 
+    									Model model) {
+        this.validator.validate(allievo, bindingResult);
+        
+        if (this.allievoService.existsByEmail(allievo.getEmail())) {
+            model.addAttribute("exists", "Allievo già esistente");
+            return "form-allievo";
+        }
+        else {
+            if (!bindingResult.hasErrors()) {
+                this.allievoService.save(allievo);
+                model.addAttribute("allievi", this.allievoService.findAll());
+                return "lista-allievo";
+            }
+        }
+        return "form-allievo";
+    }
 
 }
